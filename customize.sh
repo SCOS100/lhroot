ae7d876960ea82511023589a2ceb2f05eab5c429
Oscar Allegro <hsc100zz@gmail.com>
2024-10-26 22:50:35 +0200

    new file:   META-INF/com/google/android/update-binary 	new file:   META-INF/com/google/android/updater-script 	new file:   customize.sh 	new file:   mod-util.sh 	new file:   module.prop 	new file:   system/bin/bootlinux 	new file:   system/bin/bootlinux_env 	new file:   system/bin/bootlinux_init 	new file:   system/bin/bootlinux_log 	new file:   system/bin/killlinux 	new file:   system/bin/lhroot 	new file:   system/bin/make_image 	new file:   system/bin/mount_image

---
+++ customize.sh
@@ -0,0 +1,8 @@
+# Set permissions
+ui_print "- Setting permissions"
+set_perm $MODPATH/system/bin/make_image 0 0 0755
+set_perm $MODPATH/system/bin/mount_image 0 0 0755
+set_perm $MODPATH/system/bin/lhroot 0 0 0777
+set_perm $MODPATH/system/bin/bootlinux 0 0 0755
+set_perm $MODPATH/mod-util.sh 0 0 0777
+set_perm $MODPATH/system/bin/killlinux 0 0 0755
