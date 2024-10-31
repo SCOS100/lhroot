d5559b482b7d61de613f75e3a9aca4f59b90fea1
Oscar Allegro <hsc100zz@gmail.com>
2024-10-31 13:29:18 +0100

    modified:   mod-util.sh 	modified:   system/bin/lhroot

--- mod-util.sh
+++ mod-util.sh
@@ -178,7 +178,7 @@ div="${Bl}$(printf '%*s' "${character_no}" '' | tr " " "=")${N}"
 title_div() {
   [ "$1" == "-c" ] && local character_no=$2 && shift 2
   [ -z "$1" ] && { local message=; no=0; } || { local message="$@ "; local no=$(echo "$@" | wc -c); }
-  [ $character_no -gt $no ] && local extdiv=$((character_no-no)) || { echo "Invalid!"; return; }
+  [ $character_no -gt $no ] && local extdiv=$(( ${character_no} - ${no} ))
   echo "${W}$message${N}${Bl}$(printf '%*s' "$extdiv" '' | tr " " "=")${N}"
 }
 
